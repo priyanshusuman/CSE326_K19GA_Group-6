b9f63d1515fdbfbd240a87da2b859ea7906de92d
priyanshu <54205429+priyanshusuman@users.noreply.github.com>
2020-04-09 19:00:45 +0530

    fitnessClub
    This is our project on fitness club.

---
+++ New folder/JavaScript.js
@@ -0,0 +1,15 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "flex";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function openPop() {
+  document.getElementById("pop").style.display = "flex";
+}
+
+function closePop() {
+  document.getElementById("pop").style.display = "none";
+}
